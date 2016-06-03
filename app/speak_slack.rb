a0c71f36128985d97efb4e381aeb209d980c3426
Travis Spangle <travis.spangle@k2sports.com>
2016-06-03 09:22:50 -0700

    SlackSpeak provides a message

--- app/speak_slack.rb
+++ app/speak_slack.rb
@@ -1,6 +1,14 @@
 # Takes an array of food truck names and formats it to slack
+require_relative '../constants'
 
 module FoodTruck
   class SpeakSlack
+    def self.trucks(trucks: [])
+      msg = ""
+      trucks.each do |truck|
+        msg << "\n#{truck}"
+      end
+      ( msg.empty? ? MSG_NO_FOOD_TRUCKS : msg.strip )
+    end
   end
 end
--- spec/unit/speak_slack_spec.rb
+++ spec/unit/speak_slack_spec.rb
@@ -1,13 +1,18 @@
 require_relative '../../app/speak_slack'
+require_relative '../../constants'
 
 module FoodTruck
   RSpec.describe SpeakSlack do
     describe 'SpeakSlack says' do
       context 'when passed an empty array' do
-        it 'food trucks cannot be found'
+        it 'food trucks cannot be found' do
+          expect(SpeakSlack.trucks()).to be(MSG_NO_FOOD_TRUCKS)
+        end
       end
       context 'when passed an array with food trucks' do
-        it 'lists food trucks in a slack format'
+        it 'lists food trucks in a slack format' do
+          expect(SpeakSlack.trucks(trucks:['one','two'])).to match(/one\ntwo/)
+        end
       end
     end
   end
