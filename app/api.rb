2513af08d5fa6fa986423952a9081be93c5fa68a
Travis Spangle <travis.spangle@k2sports.com>
2016-06-03 10:04:54 -0700

    Get call provides list of trucks

--- app/api.rb
+++ app/api.rb
@@ -1,1 +1,14 @@
 # Handles web request/response
+require 'sinatra/base'
+require_relative 'food_truck'
+require_relative 'speak_slack'
+
+module FoodTruck
+  class API < Sinatra::Base
+    get '/api' do
+      ft = FoodTruck.new
+
+      SpeakSlack.trucks(trucks: ft.trucks )
+    end
+  end
+end
---
+++ spec/acceptance/food_truck_api_spec.rb
@@ -0,0 +1,19 @@
+require 'rack/test'
+require_relative '../../app/api'
+
+module FoodTruck
+  RSpec.describe 'Food Truck API' do
+    include Rack::Test::Methods
+
+    def app
+      API.new
+    end
+
+    describe 'provides message' do
+      it 'of listed food trucks' do
+        get '/api'
+        expect(last_response.status).to eq(200)
+      end
+    end
+  end
+end
